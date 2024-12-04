38da342428cb50136a8f806a0fece9102848d312
luwhy <1023412173@qq.com>
2024-12-04 19:51:49 +0800

    modify testconfig

--- test/testConfig.cc
+++ test/testConfig.cc
@@ -1,10 +1,14 @@
 #include "../sylar/config.h"
 #include "../sylar/log.h"
-
+#include <yaml-cpp/yaml.h>
 sylar::ConfigVar<int>::ptr g_int_value_config =
     sylar::Config::Lookup("system.port", (int)8080, "system port");
 sylar::ConfigVar<float>::ptr g_float_value_config =
     sylar::Config::Lookup("system.port", (float)810.2, "system port");
+
+void testSylar()
+{
+}
 int main()
 {
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->getValue();
