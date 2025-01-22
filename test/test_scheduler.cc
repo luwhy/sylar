57c9b48ace6eace431bac96351056c9baf46c2a1
luwhy <30851952+luwhy@users.noreply.github.com>
2025-01-22 16:05:38 +0800

    Update test_scheduler.cc

--- test/test_scheduler.cc
+++ test/test_scheduler.cc
@@ -2,11 +2,16 @@
 
 sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
 
+void test()
+{
+    SYLAR_LOG_INFO(g_logger) << "sylar_log_info";
+}
 int main()
 {
 
     sylar::Scheduler sc;
     sc.start();
+    sc.schedule(&test);
     sc.stop();
     return 0;
 }
