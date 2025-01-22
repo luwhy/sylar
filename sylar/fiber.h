96809b885fabed2d86774a1601cea55c3ccbafe8
luwhy <30851952+luwhy@users.noreply.github.com>
2025-01-22 16:04:10 +0800

    Update fiber.h

--- sylar/fiber.h
+++ sylar/fiber.h
@@ -4,74 +4,164 @@
 #include <memory>
 #include <functional>
 #include <ucontext.h>
-#include "thread.h"
+
 namespace sylar
 {
-    /**
-     *
-     *
-     * 把自己当成只能指针
-     * 可以获取自身智能指针
-     *
-     */
+
     class Scheduler;
+
+    /**
+     * @brief 协程类
+     */
     class Fiber : public std::enable_shared_from_this<Fiber>
     {
         friend class Scheduler;
 
     public:
         typedef std::shared_ptr<Fiber> ptr;
+
+        /**
+         * @brief 协程状态
+         */
         enum State
         {
+            /// 初始化状态
             INIT,
+            /// 暂停状态
             HOLD,
+            /// 执行中状态
             EXEC,
+            /// 结束状态
             TERM,
+            /// 可执行状态
             READY,
+            /// 异常状态
             EXCEPT
         };
 
     private:
-        // 不允许创建默认构造函数
+        /**
+         * @brief 无参构造函数
+         * @attention 每个线程第一个协程的构造
+         */
         Fiber();
 
     public:
-        Fiber(std::function<void()> cb, size_t stacksize = 0);
+        /**
+         * @brief 构造函数
+         * @param[in] cb 协程执行的函数
+         * @param[in] stacksize 协程栈大小
+         * @param[in] use_caller 是否在MainFiber上调度
+         */
+        Fiber(std::function<void()> cb, size_t stacksize = 0, bool use_caller = false);
 
+        /**
+         * @brief 析构函数
+         */
         ~Fiber();
-        // 充值协程函数，并重置状态
+
+        /**
+         * @brief 重置协程执行函数,并设置状态
+         * @pre getState() 为 INIT, TERM, EXCEPT
+         * @post getState() = INIT
+         */
         void reset(std::function<void()> cb);
-        // 切换到当前协程执行
+
+        /**
+         * @brief 将当前协程切换到运行状态
+         * @pre getState() != EXEC
+         * @post getState() = EXEC
+         */
         void swapIn();
-        // 切换到后台执行
+
+        /**
+         * @brief 将当前协程切换到后台
+         */
         void swapOut();
 
+        /**
+         * @brief 将当前线程切换到执行状态
+         * @pre 执行的为当前线程的主协程
+         */
+        void call();
+
+        /**
+         * @brief 将当前线程切换到后台
+         * @pre 执行的为该协程
+         * @post 返回到线程的主协程
+         */
+        void back();
+
+        /**
+         * @brief 返回协程id
+         */
         uint64_t getId() const { return m_id; }
 
+        /**
+         * @brief 返回协程状态
+         */
+        State getState() const { return m_state; }
+
+        void setState(State state) { m_state = state; }
+
     public:
-        // 设置当前协程
+        /**
+         * @brief 设置当前线程的运行协程
+         * @param[in] f 运行协程
+         */
         static void SetThis(Fiber *f);
-        // 返回当前协程
+
+        /**
+         * @brief 返回当前所在的协程
+         */
         static Fiber::ptr GetThis();
-        // 协程切换到后台，并且设置为Ready状态
+
+        /**
+         * @brief 将当前协程切换到后台,并设置为READY状态
+         * @post getState() = READY
+         */
         static void YieldToReady();
-        // 协程切换到后台，并设置为hold状态
+
+        /**
+         * @brief 将当前协程切换到后台,并设置为HOLD状态
+         * @post getState() = HOLD
+         */
         static void YieldToHold();
-        // 总协程数
+
+        /**
+         * @brief 返回当前协程的总数量
+         */
         static uint64_t TotalFibers();
 
+        /**
+         * @brief 协程执行函数
+         * @post 执行完成返回到线程主协程
+         */
         static void MainFunc();
 
+        /**
+         * @brief 协程执行函数
+         * @post 执行完成返回到线程调度协程
+         */
+        static void CallerMainFunc();
+
+        /**
+         * @brief 获取当前协程的id
+         */
         static uint64_t GetFiberId();
 
     private:
+        /// 协程id
         uint64_t m_id = 0;
+        /// 协程运行栈大小
         uint32_t m_stacksize = 0;
-        State m_state = State::INIT;
-        // 提供所谓的用户上下文信息
+        /// 协程状态
+        State m_state = INIT;
+        /// 协程上下文
         ucontext_t m_ctx;
-
+        /// 协程运行栈指针
         void *m_stack = nullptr;
+        /// 协程运行函数
         std::function<void()> m_cb;
     };
 
